7ee3a192c165c5d9b5876b2426e3b0e14122c2cf
7bel6 <belzeebubc@gmail.com>
2024-06-19 20:37:04 +0300

    solve the issue fragment

--- react-newbie/src/App.jsx
+++ react-newbie/src/App.jsx
@@ -2,8 +2,10 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx";
 function App() {
   return(
-    <Header/>
-    <Footer />
+    <>
+      <Header/>
+      <Footer />
+    </>
   );
 }
 
