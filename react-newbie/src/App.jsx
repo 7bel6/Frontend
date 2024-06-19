58747947a86e9478d1d510f7b073bf33589c77cd
7bel6 <belzeebubc@gmail.com>
2024-06-19 20:36:23 +0300

    Creating Basic Componants and issue

--- react-newbie/src/App.jsx
+++ react-newbie/src/App.jsx
@@ -1,5 +1,10 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx";
 function App() {
-
+  return(
+    <Header/>
+    <Footer />
+  );
 }
 
 export default App
---
+++ react-newbie/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    // we can write html and javascript
+    return(
+        <header>
+            <h1>Haider</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <hr />
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
