41a2c4d7a1e2f17e130807dcbff808cee30746a3
7bel6 <belzeebubc@gmail.com>
2024-06-19 20:43:18 +0300

    Simple Demo

--- react-newbie/src/App.jsx
+++ react-newbie/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header.jsx"
+import Food from "./Food.jsx"
 import Footer from "./Footer.jsx";
 function App() {
   return(
     <>
       <Header/>
+      <Food />
       <Footer />
     </>
   );
